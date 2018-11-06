6a506c2e58c92da185643257321598ca70b2ba8e
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-11-07 01:53:06 +0900

    [ADD] 音声ボタン追加

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
     video.addEventListener('ended', function() {
       location.href = '/message';
     });
+
+    var soundOff = document.getElementsByClassName('c-sound_off')[0];
+    var soundUp = document.getElementsByClassName('c-sound_up')[0];
+
+    soundOff.addEventListener('click', function () {
+      soundOff.style.display = 'none';
+      soundUp.style.display = 'block';
+      video.muted= false;
+    });
+
+    soundUp.addEventListener('click', function () {
+      soundUp.style.display = 'none';
+      soundOff.style.display = 'block';
+      video.muted= true;
+    });
   }
 });
 
