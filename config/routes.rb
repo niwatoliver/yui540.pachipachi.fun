2978e15b613372842098aa2c9a0271af0c922bf3
minakawa-daiki <minakawa.daiki.as@gmail.com>
2018-11-08 00:26:03 +0900

    [ADD] 404リダイレクト Closes #2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get  '/message' => 'home#message', as: :message
   post '/message' => 'home#message_create', as: :message_create
+
+  get  '*path'    => redirect('/message')
 end
